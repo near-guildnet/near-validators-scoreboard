f4b270ab83118920b4db56a7b16aa3245e6b524c
Jason <64104643+rickrods@users.noreply.github.com>
2021-01-21 23:02:44 -0600

    Update scoreboard.py

--- src/scoreboard.py
+++ src/scoreboard.py
@@ -2,16 +2,16 @@ import os
 import pandas as pd
 
 # Aggregate the numbers
-os.system('/usr/bin/node /home/rickrods/scoreboard/src/aggregate-scoreboard.js')
+os.system('/usr/bin/node /path_to-->/src/aggregate-scoreboard.js')
 
 # Remove old html file
 os.remove("path_to-->/stats/output/validators_scoreboard.html")
 
 # create variable with csv data (last_epoch)
-scoreboard = pd.read_csv("/home/rickrods/scoreboard/stats/validators_scoreboard.csv")
+scoreboard = pd.read_csv("/path_to-->/stats/validators_scoreboard.csv")
 
 # save as html file
-scoreboard.to_html("/home/rickrods/scoreboard/stats/output/validators_scoreboard.html")
+scoreboard.to_html("/path_to-->/stats/output/validators_scoreboard.html")
 
 # assign the file to a variable (string)
 html_file = scoreboard.to_html()
